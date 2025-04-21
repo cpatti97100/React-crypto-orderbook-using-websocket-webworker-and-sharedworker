2d2aa228e097c3e283e1adaf515ee84673bc817a
cpatti97100 <carlo.patti@outlook.com>
2025-04-21 18:37:25 +0200

    Format and clean up code in constants.ts for consistency

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,36 +1,38 @@
+/** @format */
+
 const PI_XBTUSDgroupings = [
-    {
-        values: [0],
-        label: '0.50',
-    },
-    {
-        values: [0.5],
-        label: '1',
-    },
-    {
-        values: [0.5, 2],
-        label: '2.50',
-    },
-];
+  {
+    values: [0],
+    label: '0.50',
+  },
+  {
+    values: [0.5],
+    label: '1',
+  },
+  {
+    values: [0.5, 2],
+    label: '2.50',
+  },
+]
 
 const PI_ETHUSDgroupings = [
-    {
-        values: [0],
-        label: '0.05',
-    },
-    {
-        values: [0.05],
-        label: '0.1',
-    },
-    {
-        values: [0.05, 0.2],
-        label: '0.25',
-    },
-];
+  {
+    values: [0],
+    label: '0.05',
+  },
+  {
+    values: [0.05],
+    label: '0.1',
+  },
+  {
+    values: [0.05, 0.2],
+    label: '0.25',
+  },
+]
 
 const productsGroupings = {
-    PI_XBTUSD: PI_XBTUSDgroupings,
-    PI_ETHUSD: PI_ETHUSDgroupings,
-};
+  PI_XBTUSD: PI_XBTUSDgroupings,
+  PI_ETHUSD: PI_ETHUSDgroupings,
+}
 
-export { productsGroupings };
+export { productsGroupings }
