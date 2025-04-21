b5e90119ab3613646b8bdc62a23b18ca4ddf379a
cpatti97100 <carlo.patti@outlook.com>
2025-04-21 18:09:46 +0200

    Refactor webSocket.shared-worker.ts for improved formatting and consistency

--- src/providers/SharedWorkerProvider/webSocket.shared-worker.ts
+++ src/providers/SharedWorkerProvider/webSocket.shared-worker.ts
@@ -1,50 +1,51 @@
-import { OrderBook, Product } from '@src/@types';
+/** @format */
 
-import * as Comlink from 'comlink';
-import React from 'react';
+import { OrderBook, Product } from '@src/@types'
 
-import { WebsocketOrderBookConnection } from './WebsocketOrderBookConnection';
+import * as Comlink from 'comlink'
+import React from 'react'
+
+import { WebsocketOrderBookConnection } from './WebsocketOrderBookConnection'
 
 const websocketOrderBookConnections: Record<
-    Product,
-    WebsocketOrderBookConnection
+  Product,
+  WebsocketOrderBookConnection
 > = {
-    PI_XBTUSD: new WebsocketOrderBookConnection(Product.PI_XBTUSD),
-    PI_ETHUSD: new WebsocketOrderBookConnection(Product.PI_ETHUSD),
-};
+  PI_XBTUSD: new WebsocketOrderBookConnection(Product.PI_XBTUSD),
+  PI_ETHUSD: new WebsocketOrderBookConnection(Product.PI_ETHUSD),
+}
 
 function connectComponent(
-    product: Product,
-    connectionID: string,
-    proxiedSetOrderBook: React.Dispatch<React.SetStateAction<OrderBook>>,
+  product: Product,
+  connectionID: string,
+  proxiedSetOrderBook: React.Dispatch<React.SetStateAction<OrderBook>>
 ) {
-    const websocketOrderBookConnection = websocketOrderBookConnections[product];
+  const websocketOrderBookConnection = websocketOrderBookConnections[product]
 
-    websocketOrderBookConnection.addCallback(connectionID, proxiedSetOrderBook);
+  websocketOrderBookConnection.addCallback(connectionID, proxiedSetOrderBook)
 }
 
 function disconnectComponent(product: Product, connectionID: string) {
-    const websocketOrderBookConnection = websocketOrderBookConnections[product];
+  const websocketOrderBookConnection = websocketOrderBookConnections[product]
 
-    websocketOrderBookConnection.deleteCallback(connectionID);
+  websocketOrderBookConnection.deleteCallback(connectionID)
 }
 
 function triggerError() {
-    Object.keys(websocketOrderBookConnections).forEach((product: Product) => {
-        websocketOrderBookConnections[product].triggerError();
-    });
+  Object.keys(websocketOrderBookConnections).forEach((product: Product) => {
+    websocketOrderBookConnections[product].triggerError()
+  })
 }
 
 const sharedWorkerFunctions = {
-    connectComponent,
-    disconnectComponent,
-    triggerError,
-};
+  connectComponent,
+  disconnectComponent,
+  triggerError,
+}
 
-((self as unknown) as SharedWorker.SharedWorkerGlobalScope).onconnect = function (
-    event: MessageEvent,
-) {
-    const port = event.ports[0];
+;(self as unknown as SharedWorker.SharedWorkerGlobalScope).onconnect =
+  function (event: MessageEvent) {
+    const port = event.ports[0]
 
-    Comlink.expose(sharedWorkerFunctions, port);
-};
+    Comlink.expose(sharedWorkerFunctions, port)
+  }
