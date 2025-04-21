81058db002e8d9c4c121cfedc01fc48885888dcd
cpatti97100 <carlo.patti@outlook.com>
2025-04-21 18:19:58 +0200

    Format and clean up code in OrderBook.tsx for consistency

--- src/components/OrderBook/OrderBook.tsx
+++ src/components/OrderBook/OrderBook.tsx
@@ -1,26 +1,28 @@
-import { Product } from "@src/@types";
+/** @format */
 
-import cn from "classnames";
-import { useRef, useState } from "react";
+import { Product } from '@src/@types'
 
-import { OrderBookHeader } from "../OrderBookHeader";
-import { OrderBookPanel } from "../OrderBookPanel";
-import { productsGroupings } from "../constants";
-import { useOrderBookResize, useWorkerData } from "./hooks";
-import { orderBookContainer } from "./orderBook.module.scss";
-import { getGroupedOrderBook } from "./util";
+import cn from 'classnames'
+import { useRef, useState } from 'react'
+
+import { OrderBookHeader } from '../OrderBookHeader'
+import { OrderBookPanel } from '../OrderBookPanel'
+import { productsGroupings } from '../constants'
+import { useOrderBookResize, useWorkerData } from './hooks'
+import { orderBookContainer } from './orderBook.module.scss'
+import { getGroupedOrderBook } from './util'
 
 function OrderBook() {
   const [groupingValues, setGroupingValues] = useState(
     productsGroupings.PI_XBTUSD[0].values
-  );
+  )
 
-  const [product, setProduct] = useState<Product>(Product.PI_XBTUSD);
+  const [product, setProduct] = useState<Product>(Product.PI_XBTUSD)
 
-  const orderBook = useWorkerData(product);
-  const groupedOrderBook = getGroupedOrderBook(orderBook, groupingValues);
-  const orderBookRef = useRef<HTMLDivElement>(null);
-  const isMobileView = useOrderBookResize(orderBookRef);
+  const orderBook = useWorkerData(product)
+  const groupedOrderBook = getGroupedOrderBook(orderBook, groupingValues)
+  const orderBookRef = useRef<HTMLDivElement>(null)
+  const isMobileView = useOrderBookResize(orderBookRef)
 
   return (
     <section>
@@ -30,20 +32,23 @@ function OrderBook() {
         groupingValues={groupingValues}
         setGroupingValues={setGroupingValues}
       />
-      <div className={cn(orderBookContainer)} ref={orderBookRef}>
+      <div
+        className={cn(orderBookContainer)}
+        ref={orderBookRef}
+      >
         <OrderBookPanel
           isMobileView={isMobileView}
-          theme="red"
+          theme='red'
           groupedValues={groupedOrderBook.groupedAsks}
         />
         <OrderBookPanel
           isMobileView={isMobileView}
-          theme="green"
+          theme='green'
           groupedValues={groupedOrderBook.groupedBids}
         />
       </div>
     </section>
-  );
+  )
 }
 
-export default OrderBook;
+export default OrderBook
