8c2072fffbcd53789e882c1148603d2fa478e0f3
cpatti97100 <carlo.patti@outlook.com>
2025-04-21 18:15:40 +0200

    Format and clean up code in hooks.ts for consistency

--- src/components/OrderBook/hooks.ts
+++ src/components/OrderBook/hooks.ts
@@ -1,78 +1,80 @@
-import { OrderBook, Product } from '@src/@types';
-import { SharedWorkerFunctionsContext } from '@src/providers';
+/** @format */
 
-import * as Comlink from 'comlink';
-import { debounce, throttle } from 'lodash';
-import { nanoid } from 'nanoid';
-import { RefObject, useContext, useEffect, useState } from 'react';
+import { OrderBook, Product } from '@src/@types'
+import { SharedWorkerFunctionsContext } from '@src/providers'
+
+import * as Comlink from 'comlink'
+import { debounce, throttle } from 'lodash'
+import { nanoid } from 'nanoid'
+import { RefObject, useContext, useEffect, useState } from 'react'
 
 const useWorkerData = (product: Product): OrderBook => {
-    const sharedWorkerFunctions = useContext(SharedWorkerFunctionsContext);
-    const [orderBook, setOrderBook] = useState<OrderBook>({
-        asks: [],
-        bids: [],
-        maxSizeTotal: 0,
-    });
+  const sharedWorkerFunctions = useContext(SharedWorkerFunctionsContext)
+  const [orderBook, setOrderBook] = useState<OrderBook>({
+    asks: [],
+    bids: [],
+    maxSizeTotal: 0,
+  })
 
-    useEffect(() => {
-        const throttledSetOrderBook = throttle((orderBook: OrderBook) => {
-            setOrderBook(orderBook);
-        }, 2000);
+  useEffect(() => {
+    const throttledSetOrderBook = throttle((orderBook: OrderBook) => {
+      setOrderBook(orderBook)
+    }, 2000)
 
-        const proxiedSetOrderBook = Comlink.proxy(throttledSetOrderBook);
+    const proxiedSetOrderBook = Comlink.proxy(throttledSetOrderBook)
 
-        const connectionID = nanoid();
+    const connectionID = nanoid()
 
-        sharedWorkerFunctions.connectComponent(
-            product,
-            connectionID,
-            proxiedSetOrderBook,
-        );
+    sharedWorkerFunctions.connectComponent(
+      product,
+      connectionID,
+      proxiedSetOrderBook
+    )
 
-        const disconnectComponent = () => {
-            sharedWorkerFunctions.disconnectComponent(product, connectionID);
-            throttledSetOrderBook.cancel();
-        };
+    const disconnectComponent = () => {
+      sharedWorkerFunctions.disconnectComponent(product, connectionID)
+      throttledSetOrderBook.cancel()
+    }
 
-        window.addEventListener('pagehide', disconnectComponent);
+    window.addEventListener('pagehide', disconnectComponent)
 
-        return () => {
-            window.removeEventListener('pagehide', disconnectComponent);
-            disconnectComponent();
-        };
-    }, [product]);
+    return () => {
+      window.removeEventListener('pagehide', disconnectComponent)
+      disconnectComponent()
+    }
+  }, [product])
 
-    return orderBook;
-};
+  return orderBook
+}
 
 const useOrderBookResize = (
-    orderBookRef: RefObject<HTMLDivElement>,
+  orderBookRef: RefObject<HTMLDivElement>
 ): boolean => {
-    const [isMobileView, setIsMobileView] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false)
 
-    useEffect(() => {
-        const orderBookEl = orderBookRef.current;
-        const handleResize = debounce((entries: Array<ResizeObserverEntry>) => {
-            const { width } = entries[0].contentRect;
+  useEffect(() => {
+    const orderBookEl = orderBookRef.current
+    const handleResize = debounce((entries: Array<ResizeObserverEntry>) => {
+      const { width } = entries[0].contentRect
 
-            setIsMobileView(width < 600);
-        }, 250);
+      setIsMobileView(width < 600)
+    }, 250)
 
-        const resizeObserver = new ResizeObserver(handleResize);
+    const resizeObserver = new ResizeObserver(handleResize)
 
-        if (orderBookEl) {
-            resizeObserver.observe(orderBookEl);
+    if (orderBookEl) {
+      resizeObserver.observe(orderBookEl)
 
-            return () => {
-                resizeObserver.disconnect();
-                handleResize.cancel();
-            };
-        }
+      return () => {
+        resizeObserver.disconnect()
+        handleResize.cancel()
+      }
+    }
 
-        return;
-    }, [orderBookRef]);
+    return
+  }, [orderBookRef])
 
-    return isMobileView;
-};
+  return isMobileView
+}
 
-export { useWorkerData, useOrderBookResize };
+export { useWorkerData, useOrderBookResize }
