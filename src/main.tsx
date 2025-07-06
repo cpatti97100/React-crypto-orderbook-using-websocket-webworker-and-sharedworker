dc5a336abcdcb7721b77e5798b94f33fde0e47ee
cpatti97100 <carlo.patti@outlook.com>
2025-07-06 17:09:55 +0200

    Add App component and associated styles for the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/app/App.tsx'
+import App from './components/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
